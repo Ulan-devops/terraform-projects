d3d7804e048f046ae4a2a7250361baf7caf3d479
root <root@DESKTOP-7SKTBOM.localdomain>
2019-12-16 16:56:45 -0500

    nginx project deleted

--- nginx/main.tf
+++
@@ -1,9 +0,0 @@
-# Create a container
-resource "docker_container" "talant" {
-  image = "${docker_image.ubuntu.latest}"
-  name  = "talant"
-}
-
-resource "docker_image" "ubuntu" {
-  name = "ubuntu:latest"
-}
