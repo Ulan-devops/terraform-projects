bd8f290987f29312a35d3faef7d1aec4c46fe557
root <root@DESKTOP-7SKTBOM.localdomain>
2019-12-18 23:45:43 -0500

    container name updated

--- talant-ubuntu/main.tf
+++ talant-ubuntu/main.tf
@@ -1,9 +1,9 @@
 # Create a container
-resource "docker_container" "talant" {
-  image = docker_image.name
+resource "docker_container" "talant_container" {
+  image = docker_image.talant_ubuntu.latest
   name  = "talant"
 }
 
-resource "docker_image" "ubuntu" {
+resource "docker_image" "talant_ubuntu" {
   name = "ubuntu:latest"
 }
