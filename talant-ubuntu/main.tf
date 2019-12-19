06febaeb22b2d1ef0c55dda2b9aba20904d108ab
root <root@DESKTOP-7SKTBOM.localdomain>
2019-12-18 23:55:16 -0500

    command updated

--- talant-ubuntu/main.tf
+++ talant-ubuntu/main.tf
@@ -2,7 +2,7 @@
 resource "docker_container" "talant_container" {
   image = docker_image.talant_ubuntu.latest
   name  = "talant"
-  command = ["/usr/bin/sleep", "4800"]
+  command = ["sleep", "4800"]
 }
 
 resource "docker_image" "talant_ubuntu" {
