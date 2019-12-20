044cb6e23d81d61606499d3dc2b1f87a8e1ddb03
root <root@DESKTOP-7SKTBOM.localdomain>
2019-12-19 22:53:15 -0500

    nginx container initiated

--- nginx/nginx.tf
+++ nginx/nginx.tf
@@ -1,4 +1,14 @@
-resource "docker_image" "talant_nginx" {
+resource "docker_image" "talant_nginx_image" {
     name = "nginx:alpine"
+}
+
+resource "docker_container" "talant_nginx_container" {
+  image = docker_image.talant_nginx_image.name
+  name = "talant_nginx"
+  ports  {
+      internal = "80"
+      external = "30080"
+  }
+  
 }
 
