cb23dcd274b62d6ff984b5b1c4499e2145b50743
ktalant <uchkunfan@gmail.com>
2019-12-25 17:00:19 -0500

    s3 bucket and random id has been created

---
+++ AWS/storage/main.tf
@@ -0,0 +1,17 @@
+# Create a random id
+resource "random_id" "tf_bucket_id" {
+  byte_lenght       = 2
+}
+# create a bucket
+resource "aws_s3_bucket" "tf_code" {
+  bucket                = var.project_name-random_id.tf_bucket_id.dec
+  acl                   = true
+  force_destroy         = true
+
+  tags = {
+      Name              = "tf_bucket"
+  }
+}
+
+
+
