bea9dad0470ee58670b4aaadefe9d064c3e1e883
ktalant <uchkunfan@gmail.com>
2019-12-25 18:21:34 -0500

    updated

--- AWS/storage/main.tf
+++ AWS/storage/main.tf
@@ -5,7 +5,7 @@ resource "random_id" "tf_bucket_id" {
 # create a bucket
 resource "aws_s3_bucket" "tf_code" {
   bucket                = "${var.project_name}-${random_id.tf_bucket_id.dec}"
-  acl                   = true
+  acl                   = "private"
   force_destroy         = true
 
   tags = {
