323ea2e95e2aa483d7e44786c6cc74b50e897510
ktalant <uchkunfan@gmail.com>
2019-12-25 18:39:41 -0500

    updated

---
+++ AWS/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region                = "${var.aws_region}"
+}
+
+module "storage" {
+  source                = "./storage"
+  project_name          = "${var.project_name}"
+  
+}
+
