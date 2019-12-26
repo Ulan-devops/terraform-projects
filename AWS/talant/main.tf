797518b993186e5cb38078f3d65b978aaf99e2d7
ktalant <uchkunfan@gmail.com>
2019-12-25 23:43:52 -0500

    acl added

--- AWS/talant/main.tf
+++ AWS/talant/main.tf
@@ -4,5 +4,6 @@ resource "random_id" "talant_bucket_id" {
 
 resource "aws_s3_bucket" "talant_bucket" {
     bucket              =  "${var.talant_bucket_name}-${random_id.talant_bucket_id.dec}"
+    acl                 = "private"
 }
 
