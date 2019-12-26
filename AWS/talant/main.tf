2292a172421557e843f32adf820e0667cc481fed
ktalant <uchkunfan@gmail.com>
2019-12-25 23:43:16 -0500

    bucket name has been updated

--- AWS/talant/main.tf
+++ AWS/talant/main.tf
@@ -3,6 +3,6 @@ resource "random_id" "talant_bucket_id" {
 }
 
 resource "aws_s3_bucket" "talant_bucket" {
-    bucket              =  "${var.talant_bucket_name}-${random_id.talant_bucket_id}"
+    bucket              =  "${var.talant_bucket_name}-${random_id.talant_bucket_id.dec}"
 }
 
